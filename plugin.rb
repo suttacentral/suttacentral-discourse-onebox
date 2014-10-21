b728518eaa0ce56e94ada850d6d5087b2ab13dc4
Bhante Nandiya <bhante.nandiya@gmail.com>
2014-10-22 10:14:19 +1100

    Actually added file heheh

---
+++ plugin.rb
@@ -0,0 +1,23 @@
+# name: suttacentral-discourse-onebox
+# about: For generating pretty oneboxes for suttacentral.net
+# version: 0.1
+# authors: Nandiya Bhikkhu
+
+Onebox = Onebox
+
+module Onebox
+  module Engine
+    class SuttacentralOnebox
+      include Engine
+      REGEX = /^https?:\/\/suttacentral\.net\/([\w-]+)/
+      matches_regexp REGEX
+
+      def uid
+        @url.match(REGEX)[1]
+      end
+
+      def to_html
+        "<iframe width='600' height='600' frameborder='0' src='http://suttacentral.net/#{uid}?noheaderfooter=1'></iframe>"
+      end
+    end
+  end
